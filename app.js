ace0ec548e15c284d60c79350fc4e98125e152fb
aelhussiny <aelhussiny@esri.com>
2018-12-07 21:05:26 +0400

    Ping, Registration, Login, UpdateAddress, AddIdentity, DeleteIdentity

--- app.js
+++ app.js
@@ -14,13 +14,17 @@ let centralserver = "";
 
 let servercommunicationkey = new NodeRSA().generateKeyPair(1024);
 let serversigningkey = new NodeRSA().generateKeyPair(1024);
+let centralservercommunicationkey;
+let centralserversigningkey;
 
-request(process.argv[2] + "/ping", (err, serres, body) => {
-    if (err) {
+request.post(process.argv[2] + "/ping", (err, serres, body) => {
+    if (err || serres.statusCode !== 200) {
         console.error("Problem connecting to central server " + process.argv[2]);
         process.exit();
     } else {
         centralserver = process.argv[2];
+        centralservercommunicationkey = new NodeRSA(JSON.parse(body).keys.communication);
+        centralserversigningkey = new NodeRSA(JSON.parse(body).keys.signing);
     }
 });
 
@@ -40,17 +44,49 @@ app.all('/ping', (req, res) => {
     });
 });
 
+const sign = (msg) => {
+    return serversigningkey.encryptPrivate(msg, 'base64');
+}
+
+const encryptForFE = (msg) => {
+    return myprofile.appcommunicationkey.encrypt(msg, 'base64');
+}
+
+const encryptForCS = (msg) => {
+    return centralservercommunicationkey.encrypt(msg, 'base64');
+}
+
+const decryptFromFE = (msg) => {
+    return servercommunicationkey.decrypt(msg).toString('utf-8');
+}
+
+const unsignFE = (msg) => {
+    return myprofile.appsigningkey.decryptPublic(msg).toString('utf-8');
+}
+
+const unsignCS = (msg) => {
+    return centralserversigningkey.decryptPublic(msg).toString('utf-8');
+}
+
+const signAsUser = (msg) => {
+    return myprofile.signingkey.encryptPrivate(msg, 'base64');
+}
+
 app.post('/register', (req, res) => {
     try {
         const dbkey = new NodeRSA().generateKeyPair(1024);
         const signingkey = new NodeRSA().generateKeyPair(1024);
-        const username = JSON.parse(servercommunicationkey.decrypt(req.body.message).toString('utf-8')).username;
+        const decryptedmessage = JSON.parse(decryptFromFE(req.body.message));
+        const username = decryptedmessage.username;
+        const appcommunicationkey = new NodeRSA(decryptedmessage.communicationkey);
         request.post({
             url: centralserver + "/register",
             body: {
-                username: username,
-                key: signingkey.exportKey('public'),
-                address: getIP() + ":" + app.get('port')
+                message: JSON.stringify({
+                    username: decryptedmessage.username,
+                    key: signingkey.exportKey('public'),
+                    address: getIP() + ":" + app.get('port')
+                })
             },
             json: true
         }, (err, serres, body) => {
@@ -61,26 +97,34 @@ app.post('/register', (req, res) => {
                     yasmscode: 2
                 });
             } else {
-                sqlite.connect("./db/" + username + ".enc", dbkey.exportKey('private'));
-                sqlite.run("CREATE TABLE MYSIGNINGKEY(key TEXT PRIMARY KEY);", (resp) => {
-                    if (resp.error)
-                        throw res.error;
-                });
-                sqlite.run("CREATE TABLE MYIDENTITIES(identityname TEXT PRIMARY KEY);", (resp) => {
-                    if (resp.error)
-                        throw res.error;
-                });
-                sqlite.insert("MYSIGNINGKEY", {
-                    key: signingkey.exportKey('private')
-                }, (resp) => {
-                    if (resp.error)
-                        throw res.error;
-                });
-                sqlite.close();
-                res.send(JSON.stringify({
-                    username: username,
-                    key: dbkey.exportKey('private')
-                }));
+                if (JSON.parse(unsignCS(body)).status && JSON.parse(unsignCS(body)).status == "success") {
+                    sqlite.connect("./db/" + username + ".enc", dbkey.exportKey('private'));
+                    sqlite.run("CREATE TABLE MYSIGNINGKEY(key TEXT PRIMARY KEY);", (resp) => {
+                        if (resp.error)
+                            throw res.error;
+                    });
+                    sqlite.run("CREATE TABLE MYIDENTITIES(identityname TEXT PRIMARY KEY);", (resp) => {
+                        if (resp.error)
+                            throw res.error;
+                    });
+                    sqlite.insert("MYSIGNINGKEY", {
+                        key: signingkey.exportKey('private')
+                    }, (resp) => {
+                        if (resp.error)
+                            throw res.error;
+                    });
+                    sqlite.close();
+                    res.send(sign(appcommunicationkey.encrypt(JSON.stringify({
+                        username: username,
+                        key: dbkey.exportKey('private')
+                    }), 'base64')));
+                } else {
+                    res.status(500);
+                    res.send({
+                        ...err,
+                        yasmscode: 2
+                    });
+                }
             }
         });
     } catch (err) {
@@ -99,11 +143,11 @@ app.post('/updateaddress', (req, res) => {
                 url: centralserver + "/updateaddress",
                 body: {
                     username: myprofile.username,
-                    command: myprofile.signingkey.encryptPrivate(JSON.stringify({
+                    command: signAsUser(JSON.stringify({
                         command: "updateaddress",
                         address: getIP() + ":" + app.get('port'),
                         timestamp: (new Date()).getTime()
-                    }), 'base64')
+                    }))
                 },
                 json: true
             }, (err, serres, body) => {
@@ -114,7 +158,7 @@ app.post('/updateaddress', (req, res) => {
                         yasmscode: 2
                     });
                 } else {
-                    res.send(body);
+                    res.send(sign(encryptForFE(body)));
                 }
             });
         } else {
@@ -170,11 +214,11 @@ app.post('/receiverequestresponse', (req, res) => {
 
 app.post('/login', (req, res) => {
     try {
-        const requestcontents = JSON.parse(servercommunicationkey.decrypt(req.body.message).toString('utf-8'));
-        const keyfile = requestcontents.keyfile;
-        if (keyfile.username === requestcontents.username) {
-            if (fs.existsSync("./db/" + requestcontents.username + ".enc")) {
-                sqlite.connect("./db/" + requestcontents.username + ".enc", keyfile.key);
+        const decryptedmessage = JSON.parse(decryptFromFE(req.body.message));
+        const keyfile = decryptedmessage.keyfile;
+        if (keyfile.username === decryptedmessage.username) {
+            if (fs.existsSync("./db/" + decryptedmessage.username + ".enc")) {
+                sqlite.connect("./db/" + decryptedmessage.username + ".enc", keyfile.key);
                 sqlite.runAsync("SELECT key FROM MYSIGNINGKEY", [], (keyrows) => {
                     if (keyrows) {
                         sqlite.runAsync("SELECT identityname FROM MYIDENTITIES", [], (identityrows) => {
@@ -185,18 +229,19 @@ app.post('/login', (req, res) => {
                                 });
                             }
                         });
-                        myprofile.username = requestcontents.username;
+                        myprofile.username = decryptedmessage.username;
                         myprofile.signingkey = new NodeRSA(keyrows[0].key);
-                        myprofile.appsigningkey = new NodeRSA(requestcontents.appsigningkey);
+                        myprofile.appsigningkey = new NodeRSA(decryptedmessage.appsigningkey);
+                        myprofile.appcommunicationkey = new NodeRSA(decryptedmessage.appcommunicationkey);
                         request.post({
                             url: centralserver + "/updateaddress",
                             body: {
                                 username: myprofile.username,
-                                command: myprofile.signingkey.encryptPrivate(JSON.stringify({
+                                command: signAsUser(JSON.stringify({
                                     command: "updateaddress",
                                     address: getIP() + ":" + app.get('port'),
                                     timestamp: (new Date()).getTime()
-                                }), 'base64')
+                                }))
                             },
                             json: true
                         }, (err, serres, body) => {
@@ -207,7 +252,7 @@ app.post('/login', (req, res) => {
                                     yasmscode: 2
                                 });
                             } else {
-                                res.send(body);
+                                res.send(sign(encryptForFE(body)));
                             }
                         });
                     }
@@ -235,16 +280,17 @@ app.post('/login', (req, res) => {
 
 app.post('/addidentity', (req, res) => {
     try {
+        const decryptedmessage = JSON.parse(decryptFromFE(unsignFE(req.body.message)));
         if (myprofile.username) {
             request.post({
                 url: centralserver + "/addidentity",
                 body: {
                     username: myprofile.username,
-                    command: myprofile.signingkey.encryptPrivate(JSON.stringify({
+                    command: signAsUser(JSON.stringify({
                         command: "addidentity",
-                        identityname: req.body.identityname,
+                        identityname: decryptedmessage.identityname,
                         timestamp: (new Date()).getTime()
-                    }), 'base64')
+                    }))
                 },
                 json: true
             }, (err, serres, body) => {
@@ -256,14 +302,16 @@ app.post('/addidentity', (req, res) => {
                     });
                 } else {
                     sqlite.insert("MYIDENTITIES", {
-                        identityname: req.body.identityname
+                        identityname: decryptedmessage.identityname
                     }, (resp) => {
                         if (resp.error)
                             throw res.error;
-                        else
-                            res.send({
+                        else {
+                            myprofile.identities.push(decryptedmessage.identityname);
+                            res.send(sign(encryptForFE(JSON.stringify({
                                 "status": "success"
-                            });
+                            }))));
+                        }
                     });
                 }
             });
@@ -284,6 +332,7 @@ app.post('/addidentity', (req, res) => {
 
 app.post('/deleteidentity', (req, res) => {
     try {
+        const decryptedmessage = JSON.parse(decryptFromFE(unsignFE(req.body.message)));
         if (myprofile.username) {
             request.post({
                 url: centralserver + "/deleteidentity",
@@ -291,7 +340,7 @@ app.post('/deleteidentity', (req, res) => {
                     username: myprofile.username,
                     command: myprofile.signingkey.encryptPrivate(JSON.stringify({
                         command: "deleteidentity",
-                        identityname: req.body.identityname,
+                        identityname: decryptedmessage.identityname,
                         timestamp: (new Date()).getTime()
                     }), 'base64')
                 },
@@ -304,16 +353,20 @@ app.post('/deleteidentity', (req, res) => {
                         yasmscode: 2
                     });
                 } else {
-                    sqlite.run("DELETE FROM MYIDENTITIES WHERE identityname = ?", [req.body.identityname], (resp) => {
+                    sqlite.run("DELETE FROM MYIDENTITIES WHERE identityname = ?", [decryptedmessage.identityname], (resp) => {
                         if (res.error) {
-                            console.error(resp.error)
+                            res.status(500);
+                            res.send({
+                                ...err,
+                                yasmscode: 2
+                            });
                         } else {
-                            if (myprofile.identities.indexOf(req.body.identityname) > -1) {
+                            if (myprofile.identities.indexOf(decryptedmessage) > -1) {
                                 myprofile.identities.splice(myprofile.identities.indexOf(req.body.identityname), 1);
                             }
-                            res.send({
+                            res.send(sign(encryptForFE(JSON.stringify({
                                 "status": "success"
-                            });
+                            }))));
                         }
                     });
                 }
